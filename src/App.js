27944da1b946ea35f589556aa0ff8ffa09290c13
Ed-Affi <muhammadafaq346@gmail.com>
2020-06-13 20:37:01 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Bpp from './cod1.js'
 function App() {
   return (
     <div>
-      <h1><Bpp dish="Chicken Karahi"/></h1>
+      <h1><Bpp dish="Chicken Karahi and Pulao"/></h1>
     </div>
   );
 }
